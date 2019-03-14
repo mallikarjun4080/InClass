b185f0bd4e61aa048ee8cda28a0ef65e527ec8ec
mallikarjun <mallikarjunmadishetty1@gmail.com>
2019-03-14 11:51:32 -0400

    navigation

---
+++ src/views/MyFriends.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>My Friends Page</h1>
+    <ul>
+        <li>friend1</li>
+        <li>friend2</li>
+        <li>friend3</li>
+        <li>friend4</li>
+        <li>friend5</li>
+    </ul>
+</div>    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
